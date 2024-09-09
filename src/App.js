da2837417e30cf52f1227ed7310517b35e85835e
etesongg <etesong0114@gmail.com>
2024-09-09 23:06:51 +0900

    feat(메인페이지) : 나누기 2  (#1)

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { useState } from 'react';
 
 function App() {
   const [state, setState] = useState(0);
+  const onToggleButton = ()=> {
+    setState(state/2)
+  }
 
   return (
     <div>
+      <button onClick={onToggleButton}>버튼</button>
       <p>값 : {state}</p>
     </div>
   );
