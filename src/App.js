61f48bca6b4fde3431449cd059e514b94956ad1c
dgKim1 <kimdg3556@naver.com>
2024-09-09 23:03:47 +0900

    minus state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import { useState } from 'react';
 
 function App() {
   const [state, setState] = useState(0);
+  const minus = ()=>{
+    setState(state-1)
+  }
 
   return (
     <div>
       <p>값 : {state}</p>
+      <button onClick={minus}>빼기</button>
     </div>
   );
 }
