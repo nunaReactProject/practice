fe500c4d56172debddaca479176b4e8a9894c469
youngsik823 <youngsik823@naver.com>
2024-09-09 23:04:06 +0900

    곱하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 // CSS in JS
 
 function App() {
-  const [state, setState] = useState(0);
-
-  return (
-    <div>
-      <p>값 : {state}</p>
-    </div>
-  );
+    const [state, setState] = useState(1);
+    const multiple = () => {
+        setState(state * 2);
+    };
+    return (
+        <div>
+            <p>값 : {state}</p>
+            <button onClick={multiple}></button>
+        </div>
+    );
 }
 
 export default App;
